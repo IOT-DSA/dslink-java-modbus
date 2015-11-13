91d2e6fcdfcf6b9e6112a66190dfb2dcc3097540
Daniel Shapiro <daniel@3dpasha.com>
2015-11-13 10:01:59 -0800

    bugfix regarding null values

--- src/main/java/modbus/SlaveFolder.java
+++ src/main/java/modbus/SlaveFolder.java
@@ -389,7 +389,10 @@ public class SlaveFolder {
 		String valString = val.toString();
 		Value v = new Value(valString);
 		ValueType vt = ValueType.STRING;
-		if (val.size() == 0) return;
+		if (val.size() == 0) {
+			vt = pointNode.getValueType();
+			v = null;
+		}
 		if (val.size() == 1) {
 			valString = val.get(0).toString();
 			if (dataType == DataType.BOOLEAN) {
